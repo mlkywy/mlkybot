e7bf90156936f28185df9693df479999fa5affba
alsheikhiu <alsheikh@iu.edu>
2021-12-17 04:58:46 -0500

    components folder created

--- commands/joke.js
+++ commands/joke.js
@@ -1,18 +1,12 @@
-const fs = require("fs");
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const getRandomLine = require("../components/getRandomLine.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("joke")
-    .setDescription("Replies with a random joke."),
+    .setDescription("Replies with a joke."),
   async execute(interaction) {
-    let randomJoke = getRandomLine("./text/jokes.txt");
-    interaction.reply({ content: randomJoke });
+    let joke = getRandomLine("./text/jokes.txt");
+    interaction.reply({ content: joke });
   },
-};
-
-const getRandomLine = (filename) => {
-  let data = fs.readFileSync(filename, "utf-8");
-  let lines = data.split(/\r?\n/);
-  return lines[Math.floor(Math.random() * lines.length)];
 };
