c0dc159903ecc515adce0be2de332f6a5d8e6bf8
alsheikhiu <alsheikh@iu.edu>
2021-12-24 16:25:59 -0500

    Update getRandomLine.js

--- components/getRandomLine.js
+++ components/getRandomLine.js
@@ -6,4 +6,20 @@ const getRandomLine = (filename) => {
   return lines[Math.floor(Math.random() * lines.length)];
 };
 
+// const getRandomLinePromise = async (filename) => {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile(filename, "utf-8", (error, data) => {
+//       if (error) reject(error);
+//       else resolve(data);
+//     });
+//   });
+// };
+
+// const mainFunction = async () => {
+//   const lines = await getRandomLinePromise(process.cwd() + "/text/8ball.txt");
+//   console.log(lines);
+// };
+
+// mainFunction();
+
 module.exports = getRandomLine;
