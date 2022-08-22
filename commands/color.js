2aa5eef86f76a95d07bff61ea7392f3042e6f536
alina <mlkysheikh@gmail.com>
2022-08-22 02:01:43 -0400

    color command added + refactoring

---
+++ commands/color.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+const data = new SlashCommandBuilder()
+  .setName("color")
+  .setDescription("Generates a random color.");
+
+const execute = async (interaction) => {
+  const color = Math.floor(Math.random() * 16777215).toString(16);
+  const answer = `Hex code: #${color}`;
+  const url = `https://www.color-hex.com/color/${color}`;
+
+  const colorEmbed = new EmbedBuilder()
+    .setColor("0x" + color)
+    .setTitle("Click to view your color!")
+    .setURL(url)
+    .setDescription(answer);
+
+  interaction.reply({
+    embeds: [colorEmbed],
+  });
+};
+
+module.exports = {
+  data,
+  execute,
+};
--- commands/help.js
+++ commands/help.js
@@ -4,27 +4,27 @@ const data = new SlashCommandBuilder()
   .setName("help")
   .setDescription("View all available commands.");
 
-// Inside a command, event listener, etc.
-const helpEmbed = new EmbedBuilder()
-  .setColor(0xfeaab3)
-  .setTitle("mlkybot commands")
-  .setDescription("This bot is still in development.")
-  .addFields(
-    { name: "/help", value: "View all available commands." },
-    {
-      name: "/activity {type}",
-      value:
-        "Set up activities with your friends! (You must be in voice channel!)",
-    },
-    { name: "/8ball {question}", value: "Ask and you shall recieve." },
-    { name: "/joke", value: "Replies with a joke." }
-  )
-  .setTimestamp()
-  .setFooter({
-    text: "Made with ❤️ by Alina!",
-  });
-
 const execute = async (interaction) => {
+  const helpEmbed = new EmbedBuilder()
+    .setColor(0xfeaab3)
+    .setTitle("mlkybot commands")
+    .setDescription("This bot is still in development.")
+    .addFields(
+      { name: "/help", value: "View all available commands." },
+      {
+        name: "/activity {type}",
+        value:
+          "Set up activities with your friends! (You must be in voice channel!)",
+      },
+      { name: "/8ball {question}", value: "Ask and you shall recieve." },
+      { name: "/joke", value: "Replies with a joke." },
+      { name: "/color", value: "Generates a random color." }
+    )
+    .setTimestamp()
+    .setFooter({
+      text: "Made with ❤️ by Alina!",
+    });
+
   interaction.reply({
     embeds: [helpEmbed],
   });
