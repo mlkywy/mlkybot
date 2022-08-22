51cb99fd63ec9878a02b1f6f4b196e917e04601d
alina <mlkysheikh@gmail.com>
2022-08-21 22:40:47 -0400

    Added custom statuses

--- components/statusRotator.js
+++ components/statusRotator.js
@@ -1,11 +1,11 @@
-const { statuses } = require("../config/config.json");
+const { activities } = require("../config/config.json");
 
-const presence = ["online", "idle", "dnd"];
-const types = ["WATCHING", "LISTENING", "COMPETING"];
-let currentStatusIndex, currentPresenceIndex;
+const statuses = ["online", "idle", "dnd"];
+// const types = ["WATCHING", "LISTENING", "COMPETING"];
+let currentActivityIndex, currentStatusIndex;
 
 const statusRotator = async (Bot) => {
-  (currentPresenceIndex = 0), (currentStatusIndex = 0);
+  (currentStatusIndex = 0), (currentActivityIndex = 0);
   await setRandomStatus(Bot).then((res) => {
     return statusRotator(Bot);
   });
@@ -14,20 +14,18 @@ const statusRotator = async (Bot) => {
 const setRandomStatus = async (Bot) => {
   await Bot.user.setPresence(randomizeStatus());
   return new Promise((res) => {
-    setTimeout(res, 24000);
+    setTimeout(res, 20000);
   });
 };
 
 const randomizeStatus = () => {
+  currentActivityIndex = Math.floor(Math.random() * activities.length);
   currentStatusIndex = Math.floor(Math.random() * statuses.length);
-  currentPresenceIndex = Math.floor(Math.random() * presence.length);
-  currentTypeIndex = Math.floor(Math.random() * types.length);
+  // currentTypeIndex = Math.floor(Math.random() * types.length);
 
   const newPresence = {
-    activities: [
-      { name: statuses[currentStatusIndex], type: types[currentTypeIndex] },
-    ],
-    status: presence[currentPresenceIndex],
+    activities: [{ name: activities[currentActivityIndex] }],
+    status: statuses[currentStatusIndex],
   };
 
   return newPresence;
