42f11d5c79a0069506805dc0afc813f191762608
0xKoichi <94922954+0xKoichi@users.noreply.github.com>
2022-08-25 17:07:58 +0100

    cleanup

--- imagehandler/index.js
+++ imagehandler/index.js
@@ -1,20 +1,2 @@
 const ImageHandler = require('./imageHandler');
-
-let safebooru = new ImageHandler({
-  site: 'Safebooru',
-  nsfw: false,
-  save: true,
-  wildcard: false,
-});
-
-let gelbooru = new ImageHandler({
-  site: 'Gelbooru',
-  nsfw: false,
-  save: false,
-  wildcard: true,
-});
-
-(async () => {
-  // await safebooru.fetch('princess_zelda');
-  await gelbooru.fetch('princess_zelda');
-})();
+module.exports = ImageHandler;
